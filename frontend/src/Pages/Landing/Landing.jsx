fd16d83f94c3cd90243e5a46de4b92f6e7e675de
Abhinav Batra <56567796+AbhB2303@users.noreply.github.com>
2023-11-29 13:44:51 -0500

    minor change to landing page

--- frontend/src/Pages/Landing/Landing.jsx
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div>
+    <div style={{ textAlign: "center", margin: "300px" }}>
       <h1>Home</h1>
-      <p>You are currently logged out</p>
+      <p> Welcome to the nutrition app</p>
+      <p> Use this application to track and monitor your eating habits.</p>
+      <p>
+        You are currently logged out. To get started please log in on the top
+        right of the navigation menu.
+      </p>
     </div>
   );
 };
