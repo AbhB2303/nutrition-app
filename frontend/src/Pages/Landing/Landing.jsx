8c30db280f6d8fed88c00bae059c2eeda5bfac84
Lina Ismail <ilina19@hotmail.com>
2023-12-05 23:35:21 -0500

    landing page

--- frontend/src/Pages/Landing/Landing.jsx
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import logo from '../../assets/IMG_2181.png';
 
 const Landing = () => {
+  const navigate = useNavigate();
   return (
-    <div style={{ textAlign: "center", margin: "300px" }}>
-      <h1>Home</h1>
-      <p> Welcome to the nutrition app</p>
+    <div style={{ textAlign: "center", margin: "100px 100px 350px 100px" }}>
+      <img src={logo} alt="Logo" style={{ width: '150px' }} />
+      <h1 style={{fontSize: "45px"}}>Nutriboard</h1>
+      <p> Welcome to the nutrition dashboard app!</p>
       <p> Use this application to track and monitor your eating habits.</p>
       <p>
-        You are currently logged out. To get started please log in on the top
-        right of the navigation menu.
+        To get started please log in or create an account.
       </p>
+      <Button
+        style={{fontSize: "16px"}}
+        variant="contained"
+        onClick={() => navigate("/login")}
+       >Get started
+      </Button>
     </div>
   );
 };
